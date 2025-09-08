858b3796af9197390f534bd6b91f655b82cccd42
Mykola Kizan <mkizan@gmail.com>
2025-09-08 17:03:15 +0300

    add dummy data

---
+++ src/app/dummy-data/dummy-cartridges.ts
@@ -0,0 +1,26 @@
+export const cartridges = [
+  {
+    id: 1,
+    brand: 'Canon',
+    model: '737',
+    analog: ['737 starter L', 'HP CRG737/CF283X', 'HP 83A'],
+    printers: [''],
+    ean13: '1234567890123',
+  },
+  {
+    id: 2,
+    brand: 'Canon',
+    model: '051',
+    analog: ['HP 30A'],
+    compatiblePrinters: [''],
+    ean13: '1234567890124',
+  },
+  {
+    id: 3,
+    brand: 'Canon',
+    model: 'R-051',
+    analog: [],
+    printers: [],
+    ean13: '1234567890123',
+  },
+];
---
+++ src/app/dummy-data/dummy-printers.ts
@@ -0,0 +1,42 @@
+export const printers = [
+  {
+    id: 1,
+    brand: 'Canon',
+    model: 'mf212w',
+  },
+  {
+    id: 2,
+    brand: 'Canon',
+    model: 'mf232w',
+  },
+  {
+    id: 3,
+    brand: 'Canon',
+    model: 'mf237w',
+  },
+  {
+    id: 4,
+    brand: 'Canon',
+    model: 'mf240w',
+  },
+  {
+    id: 5,
+    brand: 'Canon',
+    model: 'mf264dw',
+  },
+  {
+    id: 6,
+    brand: 'Canon',
+    model: 'mf275w',
+  },
+  {
+    id: 7,
+    brand: 'Epson',
+    model: 'L8180',
+  },
+  {
+    id: 8,
+    brand: 'Epson',
+    model: 'L3264',
+  },
+];

8d818ff32ab2f5d6e823b86d977305ad85d5e489
Mykola Kizan <mkizan@gmail.com>
2025-10-17 08:02:09 +0300

    feat: create modal service

--- src/app/features/cartridges/components/add-cartirdge/add-cartridge.ts
+++ src/app/features/cartridges/components/add-cartirdge/add-cartridge.ts
@@ -1,48 +1,14 @@
-import {
-  Component,
-  computed,
-  effect,
-  ElementRef,
-  HostListener,
-  inject,
-  signal,
-  viewChild,
-} from '@angular/core';
-import { CartridgesService } from '../../services/cartridges-service';
+import { Component, inject } from '@angular/core';
 import { CartridgeForm } from '../cartridge-form/cartridge-form';
+import { ModalService } from '../../../../core/services/modal-service';
+import { Modal } from '../../../../shared/components/modal/modal/modal';
 
 @Component({
   selector: 'app-add-cartridge',
-  imports: [CartridgeForm],
+  imports: [CartridgeForm, Modal],
   templateUrl: './add-cartridge.html',
   styleUrl: './add-cartridge.css',
 })
 export class AddCartridge {
-  cartridgeService = inject(CartridgesService);
-  contentModalBtn = viewChild<ElementRef<HTMLDivElement>>('modalOverlay');
-
-  private _isOpen = signal(false);
-  readonly isOpen = computed(() => this._isOpen());
-
-  constructor() {
-    effect(() => {
-      const open = this._isOpen();
-      document.body.style.overflow = open ? 'hidden' : '';
-    });
-  }
-  toggleModalBtn() {
-    this._isOpen.update((value) => !value);
-  }
-  @HostListener('document:keydown.escape') handleOverlayKeydownEscape() {
-    if (this._isOpen()) this.toggleModalBtn();
-  }
-
-  ngOnDestroy() {
-    document.body.style.overflow = '';
-  }
-
-  handleOverlayClick(event: MouseEvent) {
-    if ((event.target as HTMLElement).classList.contains('modal-overlay'))
-      this.toggleModalBtn();
-  }
+  modalService = inject(ModalService);
 }

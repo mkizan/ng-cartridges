73bf376783c819e34a0f7dddc8830ced0fb6be86
Mykola Kizan <mkizan@gmail.com>
2026-04-13 09:55:56 +0300

    fix: remove lucide icons because I couldn't install angular material

--- src/app/features/cartridge-dashboard/components/counter-cartridges/counter-cartridges.ts
+++ src/app/features/cartridge-dashboard/components/counter-cartridges/counter-cartridges.ts
@@ -1,16 +1,16 @@
 import { Component, inject } from '@angular/core';
 import { CartridgesService } from '../../../cartridges/services/cartridges-service';
 import { TranslateStatusPipe } from '../../../../shared/pipes/translate-status-pipe';
-import { LucideAngularModule, ChartNoAxesCombined } from 'lucide-angular';
+// import { LucideAngularModule, ChartNoAxesCombined } from 'lucide-angular';
 
 @Component({
   selector: 'app-counter-cartridges',
-  imports: [TranslateStatusPipe, LucideAngularModule],
+  imports: [TranslateStatusPipe],
   templateUrl: './counter-cartridges.html',
   styleUrl: './counter-cartridges.css',
 })
 export class CounterCartridges {
-  readonly ChartNoAxesCombined = ChartNoAxesCombined;
+  // readonly ChartNoAxesCombined = ChartNoAxesCombined;
 
   cartridgesService = inject(CartridgesService);
   cartridges = this.cartridgesService.allCartridges;

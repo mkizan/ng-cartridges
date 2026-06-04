4f3cac60d2184dc3a68511f68d2c3140ef18d0be
Mykola Kizan <mkizan@gmail.com>
2026-06-04 22:14:28 +0300

    fix: fixed styles for cartridge-list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import { Modal } from './shared/components/modal/modal/modal';
 import { CartridgeList } from './features/cartridges/components/cartridge-list/cartridge-list';
-import { LocationList } from './features/locations/components/location-list/location-list';
 
 export const routes: Routes = [
   {
@@ -27,7 +26,6 @@ export const routes: Routes = [
   },
   {
     path: 'locations',
-    // component: LocationList
     loadComponent: () =>
       import('./features/locations/components/location-list/location-list').then(
         (m) => m.LocationList

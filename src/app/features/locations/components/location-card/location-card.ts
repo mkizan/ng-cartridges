389e2538fde53596b4a7b4d4c1583b57271e4a84
Mykola Kizan <sa.frankof01@gmail.com>
2026-06-04 16:42:55 +0300

    feat: added location card

--- src/app/features/locations/components/location-card/location-card.ts
+++ src/app/features/locations/components/location-card/location-card.ts
@@ -1,9 +1,10 @@
 import { Component, input, ChangeDetectionStrategy } from '@angular/core';
 import { ILocation } from '../../models/location-interfaces';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-location-card',
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './location-card.html',
   styleUrl: './location-card.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,

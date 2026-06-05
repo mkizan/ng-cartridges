592866549b87faff2ecec67d2673de631522e3f7
Mykola Kizan <sa.frankof01@gmail.com>
2026-06-05 13:18:39 +0300

    feat: added add location btn and location form

---
+++ src/app/features/locations/components/add-location/add-location.ts
@@ -0,0 +1,15 @@
+import { Component, inject } from '@angular/core';
+import { ModalService } from '../../../../core/services/modal/modal-service';
+import { TEXT } from '../../../../core/constants/text';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-add-location',
+  imports: [MatButtonModule],
+  templateUrl: './add-location.html',
+  styleUrl: './add-location.scss',
+})
+export class AddLocation {
+  modalService = inject(ModalService);
+  protected readonly textAddCartridge = TEXT.buttons.addCartridge;
+}

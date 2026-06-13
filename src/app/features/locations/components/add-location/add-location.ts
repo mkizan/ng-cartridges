1d1fdb734cd7a5f6fb78226d54b73d3b41bbf358
Mykola Kizan <sa.frankof01@gmail.com>
2026-06-14 00:37:25 +0300

    style: styled add location button

--- src/app/features/locations/components/add-location/add-location.ts
+++ src/app/features/locations/components/add-location/add-location.ts
@@ -2,14 +2,15 @@ import { Component, inject } from '@angular/core';
 import { ModalService } from '../../../../core/services/modal/modal-service';
 import { TEXT } from '../../../../core/constants/text';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-add-location',
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, MatIconModule],
   templateUrl: './add-location.html',
   styleUrl: './add-location.scss',
 })
 export class AddLocation {
+  protected readonly TEXT = TEXT;
   modalService = inject(ModalService);
-  protected readonly textAddCartridge = TEXT.buttons.add;
 }
